23cdcd35d87690ed33cd9f209fb2dab22e0c5db8
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-21 14:18:31 +0200

    First try at using printfwq

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints the str1 string without using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str1 = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
+}
+

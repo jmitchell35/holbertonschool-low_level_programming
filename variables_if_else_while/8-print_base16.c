b62f941506988bb2968685f79abfb888b277b479
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-22 14:30:35 +0200

    First try at 8, printing hex digits

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * main function prints the hex digits.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a < 64)
+	{
+	if (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	else
+	{
+		putchar(a + 7);
+		a++;
+	}
+	}
+	putchar(10);
+	return (0);
+}

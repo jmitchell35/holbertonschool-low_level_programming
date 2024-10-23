907708dbafe0213007e6f751c08f5fde62cf22b5
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-23 10:35:06 +0200

    First try at 0, readme

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* No std libraries allowed => no other headers required
+*
+* main - Entry point
+*
+* Main should print the string _putchar
+*
+* Restrictions :
+* All programs will be compiled with gcc 
+* using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+* Only putchar function is allowed (max 5 times, max 5 functions)
+* Code should be Betty compliant
+* Global variables not allowed
+* No more than 5 functions per file
+* Standard library not allowed
+* We are allowed to use _putchar.c as defined by papamuziko (gh)
+*
+* Return : 0 (always success)
+*/
+
+int main(void)
+{
+	char tobeprinted[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int a = 0;
+
+	while (a < 8)
+	{
+	_putchar(tobeprinted[a]);
+	a++;
+	}
+	_putchar(10);
+	return(0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project is meant to familiarize us with C functions, and nested loops.

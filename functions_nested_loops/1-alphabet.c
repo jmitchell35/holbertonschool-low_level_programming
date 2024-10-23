794de0e40f533c6810adc3ca7e280c9c9020ec2d
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-23 10:59:20 +0200

    First try at 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/*No std libraries allowed => no other headers required*/
+
+/**
+* main - Entry point
+*
+* Main should call upon the function print_alphabet.
+*
+* Restrictions :
+* All programs will be compiled with gcc
+* using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+* Only putchar function is allowed (max 5 times, max 5 functions)
+* Code should be Betty compliant
+* Global variables not allowed
+* No more than 5 functions per file
+* Standard library not allowed
+* We are allowed to use _putchar.c as defined by papamuziko (gh)
+*
+* Prototype void print_qlphqbet(void)
+* Can only use putchar twice
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}

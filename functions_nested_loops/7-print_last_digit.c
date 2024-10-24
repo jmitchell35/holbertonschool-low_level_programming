37910db7ff97c758865e54fef9eb00307df9c330
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-24 15:43:16 +0200

    First checkerr try at 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/* No std libraries allowed => no other headers required */
+
+/**
+* print_last_digit - returns last digit of a number
+*
+* @n: type int arg
+*
+* Return: int (last digit)
+*
+* Restrictions:
+* All programs will be compiled with gcc
+* using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+* Only putchar function is allowed (max 5 times, max 5 functions)
+* Code should be Betty compliant
+* Global variables not allowed
+* No more than 5 functions per file
+* Standard library not allowed
+* We are allowed to use _putchar.c as defined by papamuziko (gh)
+*
+* Prototype int print_last_digit(int)
+*/
+
+int print_last_digit(int n)
+{
+	int i = n % 10;
+	if (i < 0)
+	{
+		i = -i;
+	}
+	_putchar(i + '0');
+	return i;
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define main_h
 
 int _putchar(char c);
-int _abs(int);
+int print_last_digit(int);
 
 #endif

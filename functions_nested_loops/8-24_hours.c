f045416323e18976204ef10245559f6b05a082ff
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-25 10:19:05 +0200

    First try at 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/* No std libraries allowed => no other headers required */
+
+/**
+* jack_bauer - prints every minute of every hour ina day
+*
+*
+*
+* Return: void
+*
+* Restrictions:
+* All programs will be compiled with gcc
+* using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+* Only putchar function is allowed (max 5 times, max 5 functions)
+* Code should be Betty compliant
+* Global variables not allowed
+* No more than 5 functions per file
+* Standard library not allowed
+* We are allowed to use _putchar.c as defined by papamuziko (gh)
+*
+* Prototype void jack_bauer(void)
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+	for (minute = 0; minute < 60; minute++)
+	{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((minute / 10) + '0');
+		_putchar((minute % 10) + '0');
+		_putchar(10);
+	}
+	}
+}

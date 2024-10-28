3218379b533b559a13c5bcea0e51ef86c9fefeb2
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 14:19:24 +0100

    Correcting loop

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 	if (i % 3 == 0 && i % 5 != 0)
 		printf("%s ", "Fizz");
 	else if (i % 5 == 0 && i % 3 != 0)
 		printf("%s ", "Buzz");
-	else if (i % 5 == 0 && i % 3 == 0)
-		printf("%s", "FizzBuzz");
+	else if (i % 5 == 0 && i % 3 == 0 )
+		printf("%s ", "FizzBuzz");
 	else
 		printf("%d ", i);
 	}

eede6f43bd7c458107659fc6a2a84bbd7b384e2d
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 10:44:46 +0100

    First try ay 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* Prints numbers from 0 to 9 except 2 and 4 followed by NL */
+
+/**
+* print_numbers - function definition
+*
+*
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	if (i != 50 && i != 52)
+		_putchar(i);
+
+	_putchar(10);
+}

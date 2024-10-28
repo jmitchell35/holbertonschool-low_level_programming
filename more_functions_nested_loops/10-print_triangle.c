51391b10c8754abf83bd11f554916d8680a2c6ee
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 14:52:45 +0100

    Trying to correct output

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -19,7 +19,7 @@ void print_triangle(int size)
 	{
 	for (i = 0; i < size; i++)
 	{
-	for (j = 0; j < size - i; j++)
+	for (j = 0; j < size - (i + 1); j++)
 		_putchar(32);
 	for (k = 0; k <= i; k++)
 		_putchar(35);

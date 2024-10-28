e8b2860b18edd2b12114c881d2b5e53d46314661
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 14:11:02 +0100

    First try at 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* Fizz Buzz test */
+
+/**
+* main - Entry point
+*
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+	if (i % 3 == 0)
+		printf("%s ", "Fizz");
+	else if (i % 5 == 0)
+		printf("%s ", "Buzz");
+	else
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}

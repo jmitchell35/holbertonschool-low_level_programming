5d5ac9d60f8001be7800f02f1b9c77ef6cdf0a53
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 14:28:02 +0100

    Trying to delete extra whitespace

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 	if (i % 3 == 0 && i % 5 != 0)
-		printf("%s ", "Fizz");
+		printf("%s", "Fizz");
 	else if (i % 5 == 0 && i % 3 != 0)
-		printf("%s ", "Buzz");
-	else if (i % 5 == 0 && i % 3 == 0 )
-		printf("%s ", "FizzBuzz");
+		printf("%s", "Buzz");
+	else if (i % 5 == 0 && i % 3 == 0)
+		printf("%s", "FizzBuzz");
 	else
-		printf("%d ", i);
+		printf("%d", i);
+
+	if (i != 100)
+		printf(" ");
 	}
 	printf("\n");
 	return (0);

c0c78cd7d8d4b15d912b359a0a406d3cabd5411f
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 10:20:06 +0100

    First try at 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* Function determining if a character is a digit */
+
+/**
+* mul - function definition
+*
+* @a: first operand
+* @b: second operand
+*
+* Return: operands' product result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

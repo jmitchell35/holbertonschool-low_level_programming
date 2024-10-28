1c332a40059d8b3bf3f33ef51f1b7462815eab35
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 14:16:31 +0100

    Sticking to instructions

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,10 +14,12 @@ int main(void)
 
 	for (i = 0; i <= 100; i++)
 	{
-	if (i % 3 == 0)
+	if (i % 3 == 0 && i % 5 != 0)
 		printf("%s ", "Fizz");
-	else if (i % 5 == 0)
+	else if (i % 5 == 0 && i % 3 != 0)
 		printf("%s ", "Buzz");
+	else if (i % 5 == 0 && i % 3 == 0)
+		printf("%s", "FizzBuzz")
 	else
 		printf("%d ", i);
 	}

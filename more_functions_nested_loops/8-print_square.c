13f3d1b7cfab58f19a09504089c53b25f27c9976
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 13:55:39 +0100

    Correcting output

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -13,10 +13,13 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size == 0 || size <0)
+		_putchar(10);
+	else
 	for (i = 0; i < size; i++)
 	{
 	for (j = 0; j < size; j++)
-			_putchar(35);
+		_putchar(35);
 	_putchar(10);
 	}
 }

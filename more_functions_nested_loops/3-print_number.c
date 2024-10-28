2b59d8ea6c41cb1d346edfad1b39a50d53f544d5
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-28 10:31:57 +0100

    First try at 3

---
+++ more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* Function that prints the numbers from 0 to 9 followed by NL */
+
+/**
+* print_numbers - function definition
+*
+*
+* Return: always 0 (success)
+*/
+
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+
+	_putchar(10);
+}

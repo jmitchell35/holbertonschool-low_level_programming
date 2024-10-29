959ff9d9817140af57dd4e878fa297b8d295c751
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-29 16:22:46 +0100

    First try at 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* First function to work with pointers */
+
+/**
+* reset_to_98 - resets the value to 98 using pointer
+*
+* @n: integer
+*
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

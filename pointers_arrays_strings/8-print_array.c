ad37bee67d833cda99ac4c4827c88c55a057e6af
jmitchell35 <julian.m.mitchell@gmail.com>
2024-10-30 16:48:38 +0100

    First try at 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/* Prints n elements of an array of int, NL, printf allowed */
+
+/**
+* print_array - prints n elements
+*
+* @a: array of integers
+* @n: number of elements to print
+*
+* Return: void
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+	
+	while(i <= n - 1)
+	{
+	if (i == n - 1)
+		printf("%d\n", a[i]);
+	else
+		printf("%d, ", a[i]);
+	i++;
+	}
+}

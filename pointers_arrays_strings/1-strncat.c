251f61a199b5ad9d75bc1d06206e8fde84c1843a
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-01 13:44:02 +0100

    First try at task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/* Concatenates two strings with a byte limit */
+
+/**
+* *_strncat - appends src string to dest string adding at most n bytes
+*
+* @dest: string src is appended to
+* @src: string to add to dest
+*
+* Return: pointer to full string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *cursor;
+	char *mark;
+
+	cursor = dest;
+	while (*cursor)
+		cursor++;
+	mark = cursor;
+	while (*src && cursor < mark + n)
+	{
+		*cursor = *src;
+		cursor++;
+		src++;
+	}
+	*cursor = '\0';
+	return (dest);
+}

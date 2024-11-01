ccff11e34e12901de2ecc3a8512cb1bccf049c59
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-01 12:52:40 +0100

    First try at project 2 task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* Concatenates two strings */
+
+/**
+* *_strcat - appends src string to dest string
+*
+* @dest: string src is appended to
+* @src: string to add to dest
+*
+* Return: pointer to full string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *cursor;
+
+	cursor = dest;
+	while (*cursor)
+		cursor++;
+
+	while (*src)
+	{
+		*cursor = *src;
+		cursor++;
+		dest++;
+	}
+	*cursor = '\0';
+	return (dest);
+}

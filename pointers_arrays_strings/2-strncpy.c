a12894c0bce132d571384447a884c010f5b8a5fb
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-01 15:14:57 +0100

    First try at 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/* Copies a string as strncpy would */
+
+/**
+* *_strncpy - copies a string
+*
+* @dest: destination var for copied string
+* @src: source string
+* @n: int, number of bytes to copy
+*
+* Return: pointer to full string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *cursor, *origin_ref;
+
+	cursor = dest, origin_ref = src;
+	while (*src && src < origin_ref + n)
+	{
+		*cursor = *src;
+		src++;
+		cursor++;
+	}
+	while (cursor < dest + n)
+	{
+		*cursor = '\0';
+		cursor++;
+	}
+	return (dest);
+}

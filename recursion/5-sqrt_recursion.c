367e2919fa642a11d770a60d5a28688770d80619
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-05 21:03:11 +0100

    Betty-compliant

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,19 +2,13 @@
 /* Returns the natural square root of n (int) */
 
 /**
-* _sqrt_recursion - returns natural square root of n
-* @n: integer
-* Return: nat. sq. root of n, -1 if doesn't exist
+* side_function - makes up for static variables,
+* keeps track of incremental progress
+* @n: integer originally passed to _sqrt_recursion
+* @potential_root: int used as increment variable passed to function
+* Return: root if found, -1 if no root, calls itself to increment
 */
-
 int side_function(int n, int potential_root);
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	return side_function(n, 0);
-}
 
 int side_function(int n, int potential_root)
 {
@@ -24,4 +18,15 @@ int side_function(int n, int potential_root)
 		return (-1);
 	else
 		return (side_function(n, potential_root + 1));
+}
+/**
+* _sqrt_recursion - returns natural square root of n
+* @n: integer
+* Return: nat. sq. root of n, -1 if doesn't exist
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (side_function(n, 0));
 }
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,20 +2,14 @@
 /* Determines if number n is prime */
 
 /**
-* is_prime_number - determines if int n is prime number
-* @n: integer
+* side_function - makes up for static variable,
+* keeps track of incremental progress
+* @n: integer originally passed to is_prime_number
+* @potential_divisor: int used as incremental variable
 * Return: 1 if prime, 0 otherwise
 */
 
 int side_function(int n, int potential_divisor);
-
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-
-	return (side_function(n, 2));
-}
 
 int side_function(int n, int potential_divisor)
 {
@@ -25,4 +19,16 @@ int side_function(int n, int potential_divisor)
 		return (0);
 	else
 		return (side_function(n, potential_divisor + 1));
+}
+/**
+* is_prime_number - determines if int n is prime number
+* @n: integer
+* Return: 1 if prime, 0 otherwise
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (side_function(n, 2));
 }

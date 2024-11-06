2714ff5a8e55b487d0ae51421fd102eb68d89867
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-06 15:53:28 +0100

    Dealing with type limits

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 /* Program that multiplies two numbers */
 
 /**
@@ -21,13 +22,21 @@ int main(int argc, char *argv[])
 		printf("Error \n");
 		return (1);
 	}
-
 	operand1 = strtol(argv[1], &error_ptr1, 10);
 	operand2 = strtol(argv[2], &error_ptr2, 10);
-
 	if (*error_ptr1 != '\0' || *error_ptr2 != '\0')
 	{
 		printf("Error \n");
+		return (1);
+	}
+	if (operand1 > INT_MAX || operand1 < INT_MIN || operand2 > INT_MAX || operand2 < INT_MIN)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (operand1 != 0 && (operand2 > INT_MAX / operand1 || operand2 < INT_MIN / operand1))
+	{
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", operand1 * operand2);

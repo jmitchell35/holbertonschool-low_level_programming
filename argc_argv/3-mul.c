78d14e3c64a5a7972859b355c380571a96ea238f
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-06 15:58:39 +0100

    roll back

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -22,21 +22,13 @@ int main(int argc, char *argv[])
 		printf("Error \n");
 		return (1);
 	}
+
 	operand1 = strtol(argv[1], &error_ptr1, 10);
 	operand2 = strtol(argv[2], &error_ptr2, 10);
+
 	if (*error_ptr1 != '\0' || *error_ptr2 != '\0')
 	{
 		printf("Error \n");
-		return (1);
-	}
-	if (operand1 > INT_MAX || operand1 < INT_MIN || operand2 > INT_MAX || operand2 < INT_MIN)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if (operand1 != 0 && (operand2 > INT_MAX / operand1 || operand2 < INT_MIN / operand1))
-	{
-		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", operand1 * operand2);

f9f576a47de4c32f0b6c02c2816c595a0e0addcf
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-12 09:43:24 +0100

    betty-compliant new try with null return

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,16 +17,18 @@ int **alloc_grid(int width, int height)
 
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
-		return(NULL);
+		return (NULL);
 
 	for (i = 0; i < height; i++)
+	{
 		array[i] = (int *)malloc(width * sizeof(int));
-
+		if (array[i] == NULL)
+			return (NULL);
+	}
 	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-			if (array[i] == NULL)
-				return (NULL);
-			else
-				array[i][j] = 0;
+	{
+	for (j = 0; j < width; j++)
+		array[i][j] = 0;
+	}
 	return (array);
 }

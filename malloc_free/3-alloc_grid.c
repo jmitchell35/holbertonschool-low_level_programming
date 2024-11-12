176c8280b5bd9e60e39ba1d8880424d15a1407a7
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-12 09:29:56 +0100

    Trying initializing differently

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,20 +10,23 @@
 int **alloc_grid(int width, int height)
 {
 	int **array;
-	int i;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return(NULL);
 
 	for (i = 0; i < height; i++)
 		array[i] = (int *)malloc(width * sizeof(int));
 
-	for (i = 0; i < height * width; i++)
-		array[i] = 0;
-
-	if (array == NULL)
-		return (NULL);
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			if (array[i] == NULL)
+				return (NULL);
+			else
+				array[i][j] = 0;
 	return (array);
 }

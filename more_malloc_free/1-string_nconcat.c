a2c4dbff20d2187bfe50208dec630be6bf70b66c
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-12 18:50:07 +0100

    Another try aty calculating the size of memory to allocate

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,25 +12,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr_to_str, *str_cursor;
-	unsigned int i = 0, length1 = 0, size = 0;
+	unsigned int i = 0, length1 = 0, length2 = 0, size = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (length1 = 0; s1[length1]; length1++)
-	size = length1 + n + 1;
+	for (; s1[length1]; length1++)
+	for (; s2[length2]; length2++)
+	size = length1 + length2 - (length2 - n);
 	str_cursor = malloc(sizeof(char) * size);
 	if (str_cursor == NULL)
 		return (NULL);
 	ptr_to_str = str_cursor;
-	while (*s1 && s1 != "")
+	while (*s1)
 	{
 		*str_cursor = *s1;
 		s1++;
 		str_cursor++;
 	}
-	while (*s2 && s2 != "" && i < n)
+	while (*s2 && i < n)
 	{
 		*str_cursor = *s2;
 		s2++;

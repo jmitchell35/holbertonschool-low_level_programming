1f4b5a678827ee6a619f9ba07493ab10ba95f82a
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-13 16:47:06 +0100

    First try at 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - age, name and owner of dog
+  * @age: float, age of dog
+  * @name: char, name of
+  * @owner: name of
+  */
+typedef struct dog
+{
+	float age;
+	char *name;
+	char *owner;
+}
+
+#endif

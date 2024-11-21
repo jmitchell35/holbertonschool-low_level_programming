1ed05126fcbda44415485df3280c14235826f989
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-21 21:01:25 +0100

    Adding white space in separator's printf

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,16 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	/*va_list turns args into iterable list called args*/
 
-	if (n == 0)
-		return (0);
-
 	va_start(args, n);
 	/*va_start starts iterating*/
 	for (; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 	if (separator != NULL && i < n - 1)
-		printf("%c", *separator);
+		printf("%c ", *separator);
 	}
 	/*accessing each argument*/
 

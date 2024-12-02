cd8fcefe59a2ef3f10916b153da4fe3f62599e0a
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-02 19:51:13 +0100

    Pushing all tasks for first try at checker

--- singly_linked_list/lists.h
+++ singly_linked_list/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 typedef unsigned long int size_t;
@@ -23,5 +24,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif

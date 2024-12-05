c7ce2a90f5a4900fb71fdd760855846012c7eaba
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-05 20:02:50 +0100

    First try at task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * create_file - creates a txt file, possibly writes content into it
+  * @filename: ptr to filename to be given to new file
+  * @text_content: ptr to string to include in file (can be created empty)
+  * Return: 1 (succes), -1 (error)
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, txt_length, error_flag;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	txt_length = 0;
+	while (text_content[txt_length])
+		txt_length++;
+
+	error_flag = write(fd, text_content, txt_length);
+	if (error_flag == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}

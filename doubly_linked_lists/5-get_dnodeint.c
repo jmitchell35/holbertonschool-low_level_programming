c107eff7baf9eb2aaced818749d0ed4f4a581647
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-06 08:11:47 +0100

    Handling off range

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -7,16 +7,15 @@
   */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *cursor = head;
 	unsigned int list_iter;
 
-	if (cursor == NULL)
+	if (head == NULL)
 		return (NULL);
 	list_iter = 0;
-	while (list_iter < index)
+	while (head && list_iter < index)
 	{
-		cursor = cursor->next;
+		head = head->next;
 		list_iter++;
 	}
-	return (cursor && list_iter == index ? cursor : NULL);
+	return (head);
 }

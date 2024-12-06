24e553e45c81b791d602781cc9c9d42fe2043c18
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-06 11:49:19 +0100

    New line issue came from vi

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,15 +13,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR);
+	if (text_content == NULL)
+		return (1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
 
 	txt_length = 0;
 	while (text_content[txt_length])
@@ -29,5 +26,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	error_flag = write(fd, text_content, txt_length);
 	close(fd);
-	return (error_flag == -1 ? -1 : 1);
+	if (error_flag == -1 || error_flag != txt_length)
+		return (-1);
+
+	return (1);
 }

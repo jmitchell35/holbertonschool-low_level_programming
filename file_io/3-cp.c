f80c1103fc6e7a6fc1cd6536238ebb9ea2deac47
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-08 12:13:49 +0100

    looping to read full file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,39 +7,40 @@
   */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, error_flag;
-	char *buffer = NULL;
+	int fd_from, fd_to, bytes_read, bytes_written, error_flag;
+	char *buffer;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
-
-	while (buffer == NULL)
-		buffer = malloc(sizeof(char) * 1024);
+	buffer = malloc(sizeof(char) * 1024);
 	fd_from = open(argv[1], O_RDONLY);
-	error_flag = read(fd_from, buffer, 1024);
-	if (error_flag == -1)
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-				argv[1]);
-		free(buffer);
-		exit(98);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			free(buffer), exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer), close(fd_from), close(fd_to), exit(98);
 	}
 	error_flag = close(fd_from);
 	if (error_flag == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		free(buffer);
-		exit(100);
+		free(buffer), close(fd_to), exit(100);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	error_flag = write(fd_to, buffer, 1024);
+	error_flag = close(fd_to);
 	if (error_flag == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		free(buffer);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		free(buffer), exit(100);
 	}
-	free(buffer);
-	exit(0);
+	free(buffer), exit(0);
 }

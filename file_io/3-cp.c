2ec366e331fa48d0b126ca52758fcb2796df0793
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-08 20:30:31 +0100

    Trying another file structure to enhance error handling

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,72 +1,87 @@
 #include "main.h"
+
+#define BUFFER_SIZE 1024
+
 /**
-  * main - entry point
-  * @argc: CLI args count
-  * @argv: double-pointer to strings (CLI args)
-  * Return: int, 0 on success, error code otherwise
-  */
+ * main - Entry point
+ * @argc: int, number of arguments passed to CLI
+ * @argv: array of pointers to strings
+ * Return: 0 on success, exits with error codes otherwise
+ */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
-	char buffer[1024];
-
 	check_argc(argc);
-	if ((fd_from = open(argv[1], O_RDONLY)) == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
-	}
-	if ((fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664)) == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-	exit(99);
-	}
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		close_file_descriptor(fd_from);
-		close_file_descriptor(fd_to);
-		exit(99);
-		}
-	}
-	if (fd_from == -1 || bytes_read == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	close_file_descriptor(fd_from);
-	close_file_descriptor(fd_to);
-	exit(98);
-	}
-	close_file_descriptor(fd_from);
-	close_file_descriptor(fd_to);
-	exit(0);
+	copy_file(argv[1], argv[2]);
+	return (0);
 }
 /**
-  * close_file_descriptor - closes provided file descriptor
-  * @fd: int, file descriptor to close
-  * Return: void - exits program on error
-  */
-void close_file_descriptor(int fd)
-{
-	if ((close(fd) < 0))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-/**
-  * check_argc - checks number of args passed through CLI
-  * @argc: number of args passed through CLI
-  * Return: void - exits program on error
-  */
+ * check_argc - checks the number of arguments
+ * @argc: count of args passed through CLI, sent from main
+ */
 void check_argc(int argc)
 {
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(99);
+		exit(97);
 	}
 }
+/**
+ * copy_file - handles the actual copy operation
+ * @file_from: source file passed to CLI
+ * @file_to: destination file
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+	}
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	close_file_descriptor(fd_from);
+	exit(99);
+	}
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+	bytes_written = write(fd_to, buffer, bytes_read);
+	if (bytes_written == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	close_file_descriptor(fd_from);
+	close_file_descriptor(fd_to);
+	exit(99);
+	}
+	}
+	if (bytes_read == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	close_file_descriptor(fd_from);
+	close_file_descriptor(fd_to);
+	exit(98);
+	}
+	close_file_descriptor(fd_from);
+	close_file_descriptor(fd_to);
+}
+/**
+ * close_file_descriptor - Closes a file descriptor and checks for errors
+ * @fd: File descriptor to close
+ */
+void close_file_descriptor(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
 

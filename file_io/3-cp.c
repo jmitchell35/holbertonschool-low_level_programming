9558e36eb899854df3dbdb069aeb331bd2069e8c
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-08 19:12:58 +0100

    Trying to break down the problem

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,39 +8,55 @@
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, bytes_read, bytes_written;
-	unsigned char buffer[1024];
+	char buffer[1024];
 
-	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
-			exit(97);
+	check_argc(argc);
 	fd_from = open(argv[1], O_RDONLY);
-	bytes_read = read(fd_from, buffer, 1024);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (bytes_read > 0)
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
-		bytes_read = read(fd_from, buffer, 1024);
-		fd_to = open(argv[2], O_WRONLY | O_APPEND);
+		if (bytes_written == -1)
+			break;
 	}
-	if (bytes_read == -1 || fd_from == -1)
+
+	if (fd_from == -1 || bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from), close(fd_to), exit(98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 	}
 	if (bytes_written == -1 || fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		close(fd_from), close(fd_to), exit(99);
+		exit(99);
 	}
-	if (close(fd_from) == -1)
+	close_file_descriptor(fd_from);
+	close_file_descriptor(fd_to);
+	exit(0);
+}
+/**
+  * close_file_descriptor - closes provided file descriptor
+  * @fd: int, file descriptor to close
+  * Return: void - exits program on error
+  */
+void close_file_descriptor(int fd)
+{
+	if ((close(fd) < 0))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		close(fd_to), exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	exit(0);
+}
+/**
+  * check_argc - checks number of args passed through CLI
+  * @argc: number of args passed through CLI
+  * Return: void - exits program on error
+  */
+void check_argc(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(99);
+	}
 }
